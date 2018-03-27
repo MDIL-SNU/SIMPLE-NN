117b3700c31ab48453addeb33edb798929e01db3
Nanco-L <khlee1992@snu.ac.kr>
2018-03-27 09:22:24 +0900

    rename feature.py and add main feature

--- feature_generator.py
+++
@@ -1,6 +0,0 @@
-# Code for generating feature vector from atomic coordinates.
-# To handle atomic coordinate, ASE(Atomic Simulation Environment) is used.
-
-from ase import io
-
-def :
---
+++ features.py
@@ -0,0 +1,18 @@
+# Code for generating feature vector from atomic coordinates.
+# Currently, atomic symmetry function(reference) can available.
+# TODO: make atomic symmetry function feature generator(use C)
+
+import sys
+
+
+def generate_feature(structure_list, feature_generator):
+    feature_generator(structure_list)
+    return 0
+
+
+def main(argv=argv):
+    generate_feature(argv[0], argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv)
+
