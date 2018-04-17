3df8f5bc82fb359575c0f9b53a4446296a6786a3
Nanco-L <khlee1992@snu.ac.kr>
2018-04-17 15:58:16 +0900

    Change for multi-symmetry function parameter for different atom types

--- features/symmetry_function/feature_generator.py
+++ features/symmetry_function/feature_generator.py
@@ -21,15 +21,22 @@ def _read_params(filename):
     params_i = list()
     params_d = list()
     with open(filename, 'r') as fil:
-        atom_list = fil.readline().strip('\n').split()
         for line in fil:
             tmp = line.split()
             params_i += [list(map(int,   tmp[:3]))]
             params_d += [list(map(float, tmp[3:]))]
 
-    return params_i, params_d, atom_list
+    params_i = np.asarray(params_i, dtype=np.intc, order='C')
+    params_d = np.asarray(params_d, dtype=np.float64, order='C')
+
+    return params_i, params_d
 
 def feature_generator(structure_list, param_list):
+    # structure_list = filename which contains the directory info for each structure file
+    #   [file format]
+    #   directory1/filename [snapshot info (ASE format)]
+    # param_list = dictionary of parameter filename list. {atom_type: file_name}
+    # FIXME: change the file format
     # get directory info for each structures and parameter list of symmetry functions
 
     comm = MPI.COMM_WORLD
@@ -45,18 +52,21 @@ def feature_generator(structure_list, param_list):
 
     # parameter list
     # [symmetry function types, atom type 1, atom type 2, cutoff, parameter 1, 2, ...]
-    #params_i, params_d, atom_list = _read_params(os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/test/inp_fsymf_Ni2Si"))
-    params_i, params_d, atom_list = _read_params(param_list)
-    params_i = np.asarray(params_i, dtype=np.intc, order='C')
-    params_d = np.asarray(params_d, dtype=np.float64, order='C')
+    with open(param_list) as fil:
+        params_set = dict()
+        for line in fil:
+            tmp = line.strip('\n').split()
+            params_set[tmp[0]] = _read_params(tmp[1])
+    atom_list = params_set.keys()
 
+    """
     params_ip = _gen_2Darray_for_ffi(params_i, ffi, "int")
     params_dp = _gen_2Darray_for_ffi(params_d, ffi)
     params = np.concatenate([params_i, params_d], axis=1)
     param_num = len(params_i)
+    """
 
-    # FIXME: take directory list and CONTCAR/XDATCAR info
-    #structure_list = [os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/test/CONTCAR_Ni2Si")]
+    # read structure info
     with open(structure_list, 'r') as fil:
         structures = list()
         for line in fil:
@@ -80,49 +90,64 @@ def feature_generator(structure_list, param_list):
             scale_p = _gen_2Darray_for_ffi(scale, ffi)
             cell_p  = _gen_2Darray_for_ffi(cell, ffi)
         
+            atom_num = len(atoms.positions)
             symbols = np.array(atoms.get_chemical_symbols())
             atom_i = np.zeros([len(symbols)], dtype=np.intc, order='C')
             type_num = dict()
+            type_idx = dict()
             for j,jtem in enumerate(atom_list):
                 tmp = symbols==jtem
                 atom_i[tmp] = j+1
                 type_num[jtem] = np.sum(tmp)
+                # if atom indexs are sorted by atom type,
+                # indexs are sorted in this part.
+                # if not, it could generate bug in training process for force training
+                type_idx[jtem] = np.arange(atom_num)[tmp]
             atom_i_p = ffi.cast("int *", atom_i.ctypes.data)
 
-            atom_num = len(atoms.positions)
-
-            q = atom_num // size
-            r = atom_num %  size
-
-            begin = rank * q + min(rank, r)
-            end = begin + q
-            if r > rank:
-                end += 1
-
-            cal_atoms = np.asarray(range(begin, end), dtype=np.intc, order='C')
-            cal_num = len(cal_atoms)
-            cal_atoms_p = ffi.cast("int *", cal_atoms.ctypes.data)
-
-            x = np.zeros([cal_num, param_num], dtype=np.float64, order='C')
-            dx = np.zeros([cal_num, atom_num * param_num * 3], dtype=np.float64, order='C')
-
-            x_p = _gen_2Darray_for_ffi(x, ffi)
-            dx_p = _gen_2Darray_for_ffi(dx, ffi)
-
-            lib.calculate_sf(cell_p, cart_p, scale_p, atom_i_p, atom_num, cal_atoms_p, cal_num, params_ip, params_dp, param_num, x_p, dx_p)
-            comm.barrier()
-
             res = dict()
-            res['x'] = np.array(comm.gather(x, root=0))
-            res['dx'] = np.array(comm.gather(dx, root=0))
+            res['x'] = dict()
+            res['dx'] = dict()
+            res['F'] = dict()
+            res['params'] = dict()
+            res['N'] = type_num
+            res['E'] = atoms.get_total_energy()
 
-            if rank == 0:
-                res['x'] = np.concatenate(res['x'], axis=0).reshape([atom_num, param_num])
-                res['dx'] = np.concatenate(res['dx'], axis=0).reshape([atom_num, param_num, atom_num, 3])
-                res['E'] = atoms.get_total_energy()
-                res['F'] = atoms.get_forces()
-                res['N'] = type_num
-                res['params'] = params
+            for j,jtem in enumerate(atom_list):
+                q = type_num[jtem] // size
+                r = type_num[jtem] %  size
+
+                begin = rank * q + min(rank, r)
+                end = begin + q
+                if r > rank:
+                    end += 1
+
+                cal_atoms = np.asarray(type_idx[jtem][begin:end], dtype=np.intc, order='C')
+                cal_num = len(cal_atoms)
+                cal_atoms_p = ffi.cast("int *", cal_atoms.ctypes.data)
+
+                params_ip = _gen_2Darray_for_ffi(params_set[jtem][0], ffi, "int")
+                params_dp = _gen_2Darray_for_ffi(params_set[jtem][1], ffi)
+                params = np.concatenate(params_set[jtem], axis=1)
+                param_num = len(params)
+
+                x = np.zeros([cal_num, param_num], dtype=np.float64, order='C')
+                dx = np.zeros([cal_num, atom_num * param_num * 3], dtype=np.float64, order='C')
+
+                x_p = _gen_2Darray_for_ffi(x, ffi)
+                dx_p = _gen_2Darray_for_ffi(dx, ffi)
+
+                lib.calculate_sf(cell_p, cart_p, scale_p, atom_i_p, atom_num, cal_atoms_p, cal_num, params_ip, params_dp, param_num, x_p, dx_p)
+                comm.barrier()
+
+                res['x'][jtem] = np.array(comm.gather(x, root=0))
+                res['dx'][jtem] = np.array(comm.gather(dx, root=0))
+
+                if rank == 0:
+                    res['x'][jtem] = np.concatenate(res['x'][jtem], axis=0).reshape([type_num[jtem], param_num])
+                    res['dx'][jtem] = np.concatenate(res['dx'][jtem], axis=0).reshape([type_num[jtem], param_num, atom_num, 3])
+                    res['F'][jtem] = atoms.get_forces()[type_idx[jtem]]
+                    res['params'][jtem] = params
                 # FIXME: change the data structure
 
                 # FIXME: for test
@@ -138,6 +163,7 @@ def feature_generator(structure_list, param_list):
                 print(np.sum((dx_cal - dx_ref) < 1e-13))
                 """
 
+            if rank == 0:
                 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/test/test1.pickle"), "wb") as fil:
                     pickle.dump(res, fil, pickle.HIGHEST_PROTOCOL)  # TODO: directory setting?
 
--- train/run.py
+++ train/run.py
@@ -2,4 +2,15 @@ import tensorflow as tf
 import numpy as np
 from features.symmetry_function import model_nn
 
-model_nn.run()
+"""
+  sample code
+  - easy to generate
+
+inputs = read_input?(...)
+feature_generator(...) -> generate feature vector and file list for training
+with tf.Session() as sess:
+    train_run(sess, ...) -> run the training process
+"""
+#with tf.Session() as sess:
+    #model = 
+    #model_nn.run()
