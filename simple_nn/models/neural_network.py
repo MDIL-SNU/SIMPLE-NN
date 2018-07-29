fix the force loss calculation method

simple_nn/models/neural_network.py
@@ -458,53 +458,59 @@ class Neural_network(object):
                             if self.inputs['full_batch']:
                                 sess.run(train_iter.initializer)
                                 t_eloss = t_floss = 0
-                                train_total = 0
+                                train_tot_struc = train_tot_atom = 0
                                 while True:
                                     try:
                                         if self.inputs['use_force']:
                                             valid_elem, tmp_eloss, tmp_floss = sess.run([self.next_elem, self.e_loss, self.f_loss], feed_dict=train_fdict)
-                                            num_batch = valid_elem['num_seg'] - 1
-                                            t_eloss += tmp_eloss * num_batch
-                                            t_floss += tmp_floss * num_batch
+                                            num_batch_struc = valid_elem['num_seg'] - 1
+                                            num_batch_atom = np.sum(valid_elem['tot_num'])
+                                            t_eloss += tmp_eloss * num_batch_struc
+                                            t_floss += tmp_floss * num_batch_atom
+                                            train_tot_atom += num_batch_atom
                                         else:
                                             valid_elem, tmp_eloss = sess.run([self.next_elem, self.e_loss], feed_dict=train_fdict)
-                                            num_batch = valid_elem['num_seg'] - 1
-                                            t_eloss += tmp_eloss * num_batch
+                                            num_batch_struc = valid_elem['num_seg'] - 1
+                                            t_eloss += tmp_eloss * num_batch_struc
 
-                                        train_total += num_batch
+                                        train_tot_struc += num_batch_struc
                                     except tf.errors.OutOfRangeError:
-                                        t_eloss = np.sqrt(t_eloss/train_total)
+                                        t_eloss = np.sqrt(t_eloss/train_tot_struc)
                                         if self.inputs['use_force']:
-                                            t_floss = np.sqrt(t_floss*3/train_total)
+                                            t_floss = np.sqrt(t_floss*3/train_tot_atom)
                                         break
                             else:
-                                t_eloss = sess.run(self.e_loss, feed_dict=train_fdict)
-                                t_eloss = np.sqrt(t_eloss)
                                 if self.inputs['use_force']:
-                                    t_floss = sess.run(self.f_loss, feed_dict=train_fdict)
+                                    t_eloss, t_floss = sess.run([self.e_loss, self.f_loss], feed_dict=train_fdict)
+                                    t_eloss = np.sqrt(t_eloss)
                                     t_floss = np.sqrt(t_floss*3)
+                                else:
+                                    t_eloss = sess.run(self.e_loss, feed_dict=train_fdict)
+                                    t_eloss = np.sqrt(t_eloss)
 
                             sess.run(valid_iter.initializer)
                             eloss = floss = 0
-                            valid_total = 0
+                            valid_tot_struc = valid_tot_atom = 0
                             while True:
                                 try:
                                     if self.inputs['use_force']:
                                         valid_elem, tmp_eloss, tmp_floss = sess.run([self.next_elem, self.e_loss, self.f_loss], feed_dict=valid_fdict)
-                                        num_batch = valid_elem['num_seg'] - 1
-                                        eloss += tmp_eloss * num_batch
-                                        floss += tmp_floss * num_batch
+                                        num_batch_struc = valid_elem['num_seg'] - 1
+                                        num_batch_atom = np.sum(valid_elem['tot_num'])
+                                        eloss += tmp_eloss * num_batch_struc
+                                        floss += tmp_floss * num_batch_atom
+                                        valid_tot_atom += num_batch_atom
                                     else:
                                         valid_elem, tmp_eloss = sess.run([self.next_elem, self.e_loss], feed_dict=valid_fdict)
-                                        num_batch = valid_elem['num_seg'] - 1
-                                        eloss += tmp_eloss * num_batch
+                                        num_batch_struc = valid_elem['num_seg'] - 1
+                                        eloss += tmp_eloss * num_batch_struc
 
-                                    valid_total += num_batch
+                                    valid_tot_struc += num_batch_struc
                                 except tf.errors.OutOfRangeError:
-                                    eloss = np.sqrt(eloss/valid_total)
+                                    eloss = np.sqrt(eloss/valid_tot_struc)
                                     result += 'E RMSE(T V) = {:6.4e} {:6.4e}'.format(t_eloss,eloss)
                                     if self.inputs['use_force']:
-                                        floss = np.sqrt(floss*3/valid_total)
+                                        floss = np.sqrt(floss*3/valid_tot_atom)
                                         result += ', F RMSE(T V) = {:6.4e} {:6.4e}'.format(t_floss,floss)
                                     break
 
@@ -534,42 +540,45 @@ class Neural_network(object):
                     test_save['NN_F'] = list()
 
                 eloss = floss = 0.
-                test_total = 0
+                test_tot_struc = test_tot_atom = 0
                 result = ' Test'
                 while True:
                     try:
                         if self.inputs['use_force']:
                             test_elem, tmp_nne, tmp_nnf, tmp_eloss, tmp_floss = \
                                 sess.run([self.next_elem, self.E, self.F, self.e_loss, self.f_loss], feed_dict=test_fdict)
-                            num_batch = test_elem['num_seg'] - 1
-                            eloss += tmp_eloss * num_batch
-                            floss += tmp_floss * num_batch
+                            num_batch_struc = test_elem['num_seg'] - 1
+                            num_batch_atom = np.sum(test_elem['tot_num'])
+                            eloss += tmp_eloss * num_batch_struc
+                            floss += tmp_floss * num_batch_atom
 
                             test_save['DFT_E'].append(test_elem['E'])
                             test_save['NN_E'].append(tmp_nne)
                             test_save['N'].append(test_elem['tot_num'])
                             test_save['DFT_F'].append(test_elem['F'])
                             test_save['NN_F'].append(tmp_nnf)
+                            
+                            test_tot_struc += num_batch_atom
                         else:
                             test_elem, tmp_nne, tmp_eloss = \
                                 sess.run([self.next_elem, self.E, self.e_loss], feed_dict=test_fdict)
-                            num_batch = valid_elem['num_seg'] - 1
-                            eloss += tmp_eloss * num_batch
+                            num_batch_struc = valid_elem['num_seg'] - 1
+                            eloss += tmp_eloss * num_batch_struc
 
                             test_save['DFT_E'].append(test_elem['E'])
                             test_save['NN_E'].append(tmp_nne)
                             test_save['N'].append(test_elem['tot_num'])
 
-                        test_total += num_batch
+                        test_tot_struc += num_batch_struc
                     except tf.errors.OutOfRangeError:
-                        eloss = np.sqrt(eloss/test_total)
+                        eloss = np.sqrt(eloss/test_tot_struc)
                         result += 'E RMSE = {:6.4e}'.format(eloss)
 
                         test_save['DFT_E'] = np.concatenate(test_save['DFT_E'], axis=0)
                         test_save['NN_E'] = np.concatenate(test_save['NN_E'], axis=0)
                         test_save['N'] = np.concatenate(test_save['N'], axis=0)
                         if self.inputs['use_force']:
-                            floss = np.sqrt(floss*3/test_total)
+                            floss = np.sqrt(floss*3/test_tot_atom)
                             result += ', F RMSE = {:6.4e}'.format(floss)
 
                             test_save['DFT_F'] = np.concatenate(test_save['DFT_F'], axis=0)